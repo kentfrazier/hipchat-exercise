8017883e1090e9b5a6adaf5188bb61c4565a75ec
Kent Frazier <kentfrazier@gmail.com>
2015-05-18 15:22:38 -0500

    Add mentions functionality.

--- message.py
+++ message.py
@@ -1,2 +1,14 @@
+import re
+
+
+MENTION_REGEX = re.compile(r'(?:^|\s)@(\w+)')
+
+
 def parse(msg_string):
-    return {}
+    mentions = MENTION_REGEX.findall(msg_string)
+    parsed = {
+        key: value
+        for key, value in (('mentions', mentions),)
+        if value
+    }
+    return parsed
--- tests.py
+++ tests.py
@@ -10,7 +10,12 @@ class MessageTestCase(TestCase):
     Base class for all message test cases.
     """
 
-    def assertMessageEqual(self, obj1, obj2, msg=None):
+    def assertMessageEqual(self, message_text, expected):
+        """Parse message string and compare to expected result."""
+        parsed = message.parse(message_text)
+        self.assertMessageDictsEqual(parsed, expected)
+
+    def assertMessageDictsEqual(self, obj1, obj2):
         """Compare two parsed messages for equality and conformance."""
         for obj in (obj1, obj2):
             self.assertIsInstance(obj1, Mapping)
@@ -27,12 +32,56 @@ class SimpleMessageTests(MessageTestCase):
 
     def test_empty_message(self):
         self.assertMessageEqual(
-            message.parse(''),
+            '',
             {},
         )
 
     def test_no_special_tokens(self):
         self.assertMessageEqual(
-            message.parse('nothing to see here.'),
+            'nothing to see here.',
+            {},
+        )
+
+class MentionsTests(MessageTestCase):
+
+    def test_simple_mention(self):
+        self.assertMessageEqual(
+            '@foobar',
+            {'mentions': ['foobar']},
+        )
+
+    def test_mention_followed_by_punctuation(self):
+        self.assertMessageEqual(
+            '@foobaz.',
+            {'mentions': ['foobaz']},
+        )
+
+    def test_nothing_returned_for_lone_at_symbol(self):
+        self.assertMessageEqual(
+            '@',
+            {},
+        )
+
+    def test_nothing_returned_for_at_symbol_with_non_word_character(self):
+        self.assertMessageEqual(
+            '@!',
+            {},
+        )
+
+    def test_multiple_mentions_match(self):
+        self.assertMessageEqual(
+            '@foo @bar @baz',
+            {'mentions': ['foo', 'bar', 'baz']},
+        )
+
+    def test_matches_with_leading_space(self):
+        self.assertMessageEqual(
+            ' @foo',
+            {'mentions': ['foo']},
+        )
+
+    def test_does_not_match_email_address(self):
+        self.assertMessageEqual(
+            'test@example.com',
             {},
         )
